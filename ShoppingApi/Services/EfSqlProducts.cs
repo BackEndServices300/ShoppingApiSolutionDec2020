c8a9b75b381f4c1ea0cad107acb7bf9cebb11953
Jeffry Gonzalez <jeff@hypertheory.com>
2020-12-07 14:40:43 -0500

    Add project files.

---
+++ ShoppingApi/Services/EfSqlProducts.cs
@@ -0,0 +1,23 @@
+﻿using ShoppingApi.Models.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingApi.Services
+{
+    public class EfSqlProducts : ILookupProducts
+    {
+        public Task<GetProductDetailsResponse> GetProductById(int id)
+        {
+            return Task.FromResult(new GetProductDetailsResponse
+            {
+                Id = id,
+                Name = "Some Product",
+                Category = "Bread",
+                Count = 1,
+                Price = 1.89M
+            });
+        }
+    }
+}
