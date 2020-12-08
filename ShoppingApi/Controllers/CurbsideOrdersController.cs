87894b37028d37ca284964b80de7e1b20755e98f
Jeffry Gonzalez <jeff@hypertheory.com>
2020-12-08 15:09:39 -0500

    before break

---
+++ ShoppingApi/Controllers/CurbsideOrdersController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShoppingApi.Data;
+using ShoppingApi.Models.CurbsideOrders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingApi.Controllers
+{
+    public class CurbsideOrdersController : ControllerBase
+    {
+        private readonly ShoppingDataContext _context;
+
+        public CurbsideOrdersController(ShoppingDataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("sync/curbsideorders")]
+        public async Task<ActionResult> SyncCurbsideOrders([FromBody] PostSyncCurbsideOrdersRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            else
+            {
+                // Save it to the DB
+                var orderToSave = new CurbsideOrder
+                {
+                    For = request.For,
+                    Items = request.Items,
+                };
+                var numberOfItems = orderToSave.Items.Split(',').Count();
+                for(var t = 0; t< numberOfItems; t++)
+                {
+                    await Task.Delay(1000);
+                }
+                orderToSave.PickupDate = DateTime.Now.AddHours(numberOfItems);
+                // return 201, Location Header, Copy of the Entity
+                _context.CurbsideOrders.Add(orderToSave);
+                await _context.SaveChangesAsync();
+
+                var response = new GetCurbsideOrderResponse
+                {
+                    Id = orderToSave.Id,
+                    For = orderToSave.For,
+                    Items = orderToSave.Items,
+                    PickupDate = orderToSave.PickupDate.Value
+                };
+                return Ok(response);
+            }
+        }
+    }
+}
---
+++ ShoppingApi/Migrations/20201208194252_Curbside2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ShoppingApi.Migrations
+{
+    public partial class Curbside2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "PickupDate",
+                table: "CurbsideOrders",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "CurbsideOrders",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "CurbsideOrders");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "PickupDate",
+                table: "CurbsideOrders",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldNullable: true);
+        }
+    }
+}
